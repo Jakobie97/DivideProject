3ab329f8c2a8342b786e75dc6a8f9b57cccfbab2
Jacob T Stockwell <JacobTStockwell@gmail.com>
2023-10-09 17:05:31 -0500

    Coding Standards Clean up
    Coding Standards Clean up
    
    Moved variable names down suggested by VS
    Renaming Variable names
    Deleted White Space

--- DivideProject/Program.cs
+++ DivideProject/Program.cs
@@ -1,41 +1,34 @@
 ﻿/*
  * Names: Jacob Stockwell, Jason Fulton  
  *  
- * 
- * 
  * Description: This project is to go through the steps of co programming with co workers, Logging files, Execption Handing, 
  * and version control practice  
  * 
+ * Varaible will have cammileCase
+ * Functions will have PascalCase
+ * 
  */
-
 
 //definitions 
 using System.Reflection.Metadata.Ecma335;
 
-string number1 = "";
-string number2 = "";
-
-double convertNumber1 = 0;
-double convertNumber2 = 0;
-double result = 0;
-
-//test
-
 //Prompts 
 Console.WriteLine("Please Enter the first Number you want to divide:");
-number1 = Console.ReadLine();
+string firstUserInput = Console.ReadLine();
 
+double convertfirstUserInput = 0;
 //Looks to make sure that the user is only putting in an Double -------------------------------- 
 try
-{ convertNumber1 = Convert.ToDouble(number1);}
-catch {
+{ convertfirstUserInput = Convert.ToDouble(firstUserInput); }
+catch
+{
 
     Console.WriteLine("Hey!!! Numbers only. We only serve ints around here. Talk to the sheriff");
     Console.WriteLine("Last chance before the program dies. Please Enter a Number: ");
-     
-    number1 = Console.ReadLine();
-    //convertNumber1 = Convert.ToDouble(number1);
-    bool success = double.TryParse(number1, out convertNumber1);
+
+    firstUserInput = Console.ReadLine();
+    //convertfirstUserInput = Convert.ToDouble(firstUserInput);
+    bool success = double.TryParse(firstUserInput, out convertfirstUserInput);
     if (success)
     {
         Console.WriteLine("Your First entry was Successflly converted to Double");
@@ -45,24 +38,23 @@ catch {
         Console.WriteLine("invalid Entry, Program is ending. Relaunch the app and try again");
         return;
     }
-
-
-   
 }
 
 
 Console.WriteLine("Please Enter the Second Number you want to divide by:");
-number2 = Console.ReadLine();
+string secondUserInput = Console.ReadLine();
 
+
+double convertSecondUserInput = 0;
 try
-{convertNumber2 = Convert.ToDouble(number2);}
+{ convertSecondUserInput = Convert.ToDouble(secondUserInput); }
 catch
 {
     Console.WriteLine("I guess you dont know how to listen or read. Enjoy digging holes.");
     Console.WriteLine("Last chance before the program dies. Please Enter a Number: ");
-    number2 = Console.ReadLine();
-    //convertNumber2 = Convert.ToDouble(number2);
-    bool success = double.TryParse(number2, out convertNumber2);
+    secondUserInput = Console.ReadLine();
+    //convertSecondUserInput = Convert.ToDouble(secondUserInput);
+    bool success = double.TryParse(secondUserInput, out convertSecondUserInput);
     if (success)
     {
         Console.WriteLine("Your second entry was Successflly converted to Double");
@@ -72,14 +64,11 @@ catch
         Console.WriteLine("invalid Entry, Program is ending. Relaunch the app and try again");
         return;
     }
-
-
 }
 //----------------------------------------------------------------------------------------------
 //Output
-
-result = convertNumber1 / convertNumber2;
-Console.WriteLine($"The numbers you entered {convertNumber1} and {convertNumber2}");
+double result = convertfirstUserInput / convertSecondUserInput;
+Console.WriteLine($"The numbers you entered {convertfirstUserInput} and {convertSecondUserInput}");
 Console.WriteLine($"Lets divide these numbers buddy. " +
-    $"Your Numbers are {convertNumber1} divided by {convertNumber2}. This in an equation format is:" +
-    $" {convertNumber1} / {convertNumber2} = {result}.");
+    $"Your Numbers are {convertfirstUserInput} divided by {convertSecondUserInput}. This in an equation format is:" +
+    $" {convertfirstUserInput} / {convertSecondUserInput} = {result}.");
