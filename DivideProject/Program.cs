0da1059811eb4316cd26c6d75787df41a8335389
Jacob T Stockwell <JacobTStockwell@gmail.com>
2023-10-11 14:35:17 -0500

    Update Program.cs

--- DivideProject/Program.cs
+++ DivideProject/Program.cs
@@ -16,10 +16,10 @@ using System.Reflection.Metadata.Ecma335;
 Console.WriteLine("Please Enter the first Number you want to divide:");
 string firstUserInput = Console.ReadLine();
 
-double convertfirstUserInput = 0;
+double convertFirstUserInput = 0;
 //Looks to make sure that the user is only putting in an Double -------------------------------- 
 try
-{ convertfirstUserInput = Convert.ToDouble(firstUserInput); }
+{ convertFirstUserInput = Convert.ToDouble(firstUserInput); }
 catch
 {
 
@@ -27,8 +27,8 @@ catch
     Console.WriteLine("Last chance before the program dies. Please Enter a Number: ");
 
     firstUserInput = Console.ReadLine();
-    //convertfirstUserInput = Convert.ToDouble(firstUserInput);
-    bool success = double.TryParse(firstUserInput, out convertfirstUserInput);
+    //convertFirstUserInput = Convert.ToDouble(firstUserInput);
+    bool success = double.TryParse(firstUserInput, out convertFirstUserInput);
     if (success)
     {
         Console.WriteLine("Your First entry was Successflly converted to Double");
@@ -39,13 +39,11 @@ catch
         return;
     }
 }
-
-
 Console.WriteLine("Please Enter the Second Number you want to divide by:");
+
 string secondUserInput = Console.ReadLine();
-
-
 double convertSecondUserInput = 0;
+
 try
 { convertSecondUserInput = Convert.ToDouble(secondUserInput); }
 catch
@@ -67,8 +65,8 @@ catch
 }
 //----------------------------------------------------------------------------------------------
 //Output
-double result = convertfirstUserInput / convertSecondUserInput;
-Console.WriteLine($"The numbers you entered {convertfirstUserInput} and {convertSecondUserInput}");
+double result = convertFirstUserInput / convertSecondUserInput;
+Console.WriteLine($"The numbers you entered {convertFirstUserInput} and {convertSecondUserInput}");
 Console.WriteLine($"Lets divide these numbers buddy. " +
-    $"Your Numbers are {convertfirstUserInput} divided by {convertSecondUserInput}. This in an equation format is:" +
-    $" {convertfirstUserInput} / {convertSecondUserInput} = {result}.");
+    $"Your Numbers are {convertFirstUserInput} divided by {convertSecondUserInput}. This in an equation format is:" +
+    $" {convertFirstUserInput} / {convertSecondUserInput} = {result}.");
